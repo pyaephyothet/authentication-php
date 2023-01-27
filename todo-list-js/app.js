b5e60fc90c60352c54c42f623a1d8eea219aa976
Pyae Phyo Thet <scm.pyaephyothet@gmail.com>
2023-01-27 08:07:29 +0630

    todo js

--- todo-list-js/app.js
+++ todo-list-js/app.js
@@ -4,13 +4,13 @@ const todo_list = document.querySelector(".todo-list");
 const option = document.querySelector(".option");
 const delete_all = document.querySelector(".delete-all");
 const check_all = document.querySelector(".check-all");
-const input_task = document.querySelector(".input-task");
+
 
 add_btn.addEventListener("click", add_task);
 todo_list.addEventListener("click", delete_task);
 option.addEventListener("click", filter_task);
 check_all.addEventListener("click", all_check);
-input_task.addEventListener("click", edit);
+
 
 function add_task(e) {
     e.preventDefault();
@@ -19,11 +19,52 @@ function add_task(e) {
         return false;
     } else {
         const list = document.createElement("div");
-        list.innerHTML = `<li class="todo-item">${task_input.value}</li><button class="input-task">edit</button><button class="complete-btn"><i class="fas fa-check"></i></button><button class="trash-btn"><i class="fas fa-trash"></i>`;
+        //list.innerHTML = `<input class="todo-item" type="button" value="${task_input.value}" ><button class="complete-btn"><i class="fas fa-check"></i></button><button class="trash-btn"><i class="fas fa-trash"></i>`;
+
+        const task_input_el = document.createElement('input');
+        task_input_el.classList.add('todo-item');
+        task_input_el.type = 'text';
+        task_input_el.value = task_input.value;
+        task_input_el.setAttribute('readonly', 'readonly');
+        list.appendChild(task_input_el);
+
+        const editButton = document.createElement("button");
+        editButton.innerText = `Edit`;
+        editButton.classList.add("edit-btn");
+        list.appendChild(editButton);
+
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+        completedButton.classList.add("complete-btn");
+        list.appendChild(completedButton);
+
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+        trashButton.classList.add("trash-btn");
+        list.appendChild(trashButton);
+
         list.classList.add("task");
         todo_list.appendChild(list);
 
         task_input.value = "";
+
+
+
+        const editInput = document.querySelector(".edit-btn");
+        editInput.addEventListener("click", edit);
+        function edit(e) {
+            const editArea = e.target;
+            const editTxt = list.firstChild;
+            if (editArea.innerText.toLowerCase() == "edit") {
+                editArea.innerText = "Save";
+                editTxt.removeAttribute("readonly");
+                editTxt.focus();
+            } else {
+                editArea.innerText = "Edit";
+                editTxt.setAttribute("readonly", "readonly");
+            }
+        }
+
     }
 
 }
@@ -78,11 +119,3 @@ function all_check(e) {
     todo_list.classList.toggle("completed");
     return;
 }
-
-
-
-
-
-
-
-
